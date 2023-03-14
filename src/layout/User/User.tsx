1a4524629fa6e91528ae07a5782712d594d18f79
Andrey <dovban.andreyy@rambler.ru>
2023-03-14 06:42:29 +0300

    реализована задержка припоиске

--- src/layout/User/User.tsx
+++ src/layout/User/User.tsx
@@ -1,15 +1,70 @@
 import styles from './User.module.css';
+import { useRef } from 'react';
 import { UserProps } from './User.props';
 import cn from 'classnames';
 import Search from './search.svg';
 import Avatar from './avatar.svg';
+import { useRecoilState } from 'recoil';
+import { searchStringState, typeSearchState } from '../../store/atoms';
 
 export default function User({ className, ...props }: UserProps) {
+    const [typeSearch, setTypeSearch] = useRecoilState(typeSearchState);
+    const [searchString, setSearchString] = useRecoilState(searchStringState);
+    const debounceRef = useRef<number>();
+
+    function handlerChangeSearch(e: any) {
+        setSearchString(e.currentTarget.value);
+        debunceSearch(e.currentTarget.value);
+    }
+
+    function handlerChangeType(e: {
+        target: { value: string | ((currVal: string) => string) };
+    }) {
+        setTypeSearch(e.target.value);
+    }
+
+    function debunceSearch(value: string) {
+        if (debounceRef.current) {
+            clearTimeout(debounceRef.current);
+        }
+        debounceRef.current = setTimeout(() => {
+            if (value.length > 2) {
+                console.log(value);
+
+                // dispatch(showSearchResult({ targetString: value }));
+            }
+        }, 500);
+    }
+
     return (
         <div className={cn(className, styles.user)} {...props}>
             <div className={styles.search}>
-                <input type='text' />
                 <Search />
+                <input
+                    className={styles.searchText}
+                    type='text'
+                    onChange={handlerChangeSearch}
+                />
+                <span>
+                    <input
+                        name='radio'
+                        value='1'
+                        checked={typeSearch === '1' ? true : false}
+                        type='radio'
+                        onChange={handlerChangeType}
+                    />{' '}
+                    поиск по автору
+                </span>
+                <span>
+                    <input
+                        name='radio'
+                        value='2'
+                        checked={typeSearch === '1' ? false : true}
+                        type='radio'
+                        onChange={handlerChangeType}
+                    />{' '}
+                    поиск по названию
+                </span>
             </div>
             <span className={styles.avatar}>
                 <Avatar />
--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -9,3 +9,13 @@ export const currentBooksState = atom({
     key: 'currentBooksState',
     default: [],
 });
+
+export const typeSearchState = atom({
+    key: 'typeSearchState',
+    default: '1',
+});
+
+export const searchStringState = atom({
+    key: 'searchStringState',
+    default: '',
+});
